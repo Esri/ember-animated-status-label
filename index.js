b5cd2b7c81b2046928ac779575159702649c9012
Timothy Morey <tmorey@esri.com>
2018-07-30 09:39:03 -0400

    removed sass dep

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict'
 
 module.exports = {
-  name: 'ember-animated-status-label'
+  name: 'ember-animated-status-label',
+
+  included(app) {
+    app.import('vendor/animated-status-label.css')
+  }
 }
